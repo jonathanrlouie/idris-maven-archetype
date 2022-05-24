2879e08fe35aec3ed6f8d78fff5b6e092a492345
jonathanrlouie <jonathanrlouie@gmail.com>
2022-05-23 17:38:57 -0700

    Add convenience alias for generating projects quickly

--- shell.nix
+++ shell.nix
@@ -1,4 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   buildInputs = with pkgs; [ maven ];
+  shellHook = ''
+    alias generate='mvn archetype:generate \
+      -DarchetypeGroupId=io.github.jonathanrlouie \
+      -DarchetypeArtifactId=idris-maven-archetype \
+      -DarchetypeVersion=1.0-SNAPSHOT \
+      -DgroupId=test \
+      -DartifactId=artifact'
+  '';
 }
